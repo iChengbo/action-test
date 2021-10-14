30ae82474509f90833039a2434a479c5d5877107
name <name@gmail.com>
2021-10-14 11:01:13 +0000

    update by github actions

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+exports.getName = void 0;
+/*
+ * @Author: iChengbo
+ * @Date: 2021-10-14 18:58:21
+ * @LastEditors: iChengbo
+ * @LastEditTime: 2021-10-14 18:59:28
+ * @FilePath: /action-test/main.ts
+ */
+var getName = function () {
+    return 'iChengbo';
+};
+exports.getName = getName;
