2be905365282ee2c4837094a7d4ffa88d91ae8f5
xiechengbo <xiechengbo@58.com>
2021-10-14 10:29:04 +0800

    test

---
+++ index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: iChengbo
+ * @Date: 2021-10-14 10:22:55
+ * @LastEditors: iChengbo
+ * @LastEditTime: 2021-10-14 10:24:07
+ * @FilePath: /action-test/index.ts
+ */
+const sayHello = (name: string) => {
+    console.log('Helloo' + name);
+}
+
+export { sayHello }
