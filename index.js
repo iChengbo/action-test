da1c760d21a0238331b3cc4dbb90c8dea0f2a4a9
name <name@gmail.com>
2021-10-14 04:06:35 +0000

    update by github actions

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+exports.sayHello = void 0;
+/*
+ * @Author: iChengbo
+ * @Date: 2021-10-14 10:22:55
+ * @LastEditors: iChengbo
+ * @LastEditTime: 2021-10-14 10:24:07
+ * @FilePath: /action-test/index.ts
+ */
+var sayHello = function (name) {
+    console.log('Helloo' + name);
+};
+exports.sayHello = sayHello;
