11e289c370f0bbcd667d57c86d7f1735344aca3b
xiechengbo <xiechengbo@58.com>
2021-10-14 18:59:43 +0800

    update code

---
+++ main.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: iChengbo
+ * @Date: 2021-10-14 18:58:21
+ * @LastEditors: iChengbo
+ * @LastEditTime: 2021-10-14 18:59:28
+ * @FilePath: /action-test/main.ts
+ */
+const getName = () => {
+    return 'iChengbo'
+}
+
+export { getName }
